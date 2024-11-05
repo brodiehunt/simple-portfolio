b299e78b404c17cbbb2e4cf71e656b21b7044e54
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-11-05 19:56:45 +1100

    Add two code sydney projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,71 +1,126 @@
 import Link from "./Link";
 import ProjectCard from "./ProjectCard";
-import designoImg from '../assets/designo.png';
-import chatterImg from '../assets/chatter.png';
-import shoppingCartImg from '../assets/shoppingCart.png';
-import memoryImg from '../assets/memory.png';
-import tictactoeImg from '../assets/tictactoe.png';
-import jarrahBuildImg from '../assets/jarrahbuild.png';
+import designoImg from "../assets/designo.png";
+import audiophileImg from "../assets/audiophile-ecommerce.png";
+import chatterImg from "../assets/chatter.png";
+import shoppingCartImg from "../assets/shoppingCart.png";
+import memoryImg from "../assets/memory.png";
+import tictactoeImg from "../assets/tictactoe.png";
+import jarrahBuildImg from "../assets/jarrahbuild.png";
+import codeSydneyImg from "../assets/code_sydney_website.webp";
+import classifiedAdsApp from "../assets/ustaa_website.webp";
 
 const projects = [
   {
-    title: 'designo studios',
-    liveSite: 'https://brodie-designo-studios.netlify.app/',
-    github: 'https://github.com/brodiehunt/designo-studio',
-    img: designoImg,
-    tech: ['react', 'styled-components', 'framer-motion', 'leafletJs', 'react-router'],
-  },
-  {
-    title: 'chatter live messenger',
-    liveSite: 'https://brodie-chatter-app.netlify.app/',
-    github: 'https://github.com/brodiehunt/live-messenger-server',
+    title: "chatter live messenger",
+    liveSite: "https://brodie-chatter-app.netlify.app/",
+    github: "https://github.com/brodiehunt/live-messenger-server",
     img: chatterImg,
-    tech: ['react', 'express', 'MongoDB', 'Passport', 'socket.io', 'express-validator', 'jest', 'mongoose', 'axios'],
+    tech: [
+      "react",
+      "express",
+      "MongoDB",
+      "Passport",
+      "socket.io",
+      "express-validator",
+      "jest",
+      "mongoose",
+      "axios",
+    ],
   },
   {
-    title: 'Retail shop - Api',
-    liveSite: 'https://brodiehunt-shopping-cart.netlify.app/',
-    github: 'https://github.com/brodiehunt/shopping-cart',
+    title: "Code.Sydney Organisation Website",
+    liveSite: "https://www.code.sydney/",
+    github: "https://github.com/codesydney/Code.Sydney-Website",
+    img: codeSydneyImg,
+    tech: [
+      "NextJS",
+      "SanityCMS",
+      "Framer Motion",
+      "React hook form",
+      "zod",
+      "emailjs",
+    ],
+  },
+  {
+    title: "audiophile ecommerce",
+    liveSite: "https://brodie-audiophile.vercel.app/",
+    github: "https://github.com/brodiehunt/audiophile",
+    img: audiophileImg,
+    tech: ["nextJS", "css modules", "express", "mongodb", "stripe", "SSG"],
+  },
+  {
+    title: "UST Alumni Australia Website",
+    liveSite: "https://www.ustaa.au/",
+    github: "https://github.com/codesydney/classified-ads-app-for-good",
+    img: classifiedAdsApp,
+    tech: [
+      "react",
+      "redux",
+      "react-router-dom",
+      "react-testing-library",
+      "tailwindcss",
+      "yup",
+      "axios",
+      "express",
+      "sendgrid",
+      "s3",
+    ],
+  },
+  {
+    title: "designo studios",
+    liveSite: "https://brodie-designo-studios.netlify.app/",
+    github: "https://github.com/brodiehunt/designo-studio",
+    img: designoImg,
+    tech: [
+      "react",
+      "styled-components",
+      "framer-motion",
+      "leafletJs",
+      "react-router",
+    ],
+  },
+  {
+    title: "Retail shop - Api",
+    liveSite: "https://brodiehunt-shopping-cart.netlify.app/",
+    github: "https://github.com/brodiehunt/shopping-cart",
     img: shoppingCartImg,
-    tech: ['react', 'framer-motion', 'API']
+    tech: ["react", "framer-motion", "API"],
   },
   {
-    title: 'Memory Game',
-    liveSite: 'https://brodiehunt-memory-game.netlify.app/',
-    github: 'https://github.com/brodiehunt/memory-game',
+    title: "Memory Game",
+    liveSite: "https://brodiehunt-memory-game.netlify.app/",
+    github: "https://github.com/brodiehunt/memory-game",
     img: memoryImg,
-    tech: ['react', 'styled-components',]
+    tech: ["react", "styled-components"],
   },
   {
-    title: 'Tic Tac Toe',
-    liveSite: 'https://brodiehunt-tic-tac-toe.netlify.app/',
-    github: 'https://github.com/brodiehunt/tic-tac-toe',
+    title: "Tic Tac Toe",
+    liveSite: "https://brodiehunt-tic-tac-toe.netlify.app/",
+    github: "https://github.com/brodiehunt/tic-tac-toe",
     img: tictactoeImg,
-    tech: ['react', 'CSS',]
+    tech: ["react", "CSS"],
   },
   {
-    title: 'JarrahBuild',
-    liveSite: 'http://www.jarrahbuild.com.au/index.html',
-    github: '',
+    title: "JarrahBuild",
+    liveSite: "http://www.jarrahbuild.com.au/index.html",
+    github: "",
     img: jarrahBuildImg,
-    tech: ['HTML', 'CSS', 'Javascript', 'Bootstrap']
-  }
-]
+    tech: ["HTML", "CSS", "Javascript", "Bootstrap"],
+  },
+];
 export default function Projects() {
-
   return (
     <section className="projects-section">
-      <header className='projects-header'>
+      <header className="projects-header">
         <h2>Projects</h2>
         <Link url="#contact">Contact me</Link>
       </header>
       <div className="projects-container">
         {projects.map((project, index) => {
-          return (
-            <ProjectCard key={index} project={project}/>
-          )
+          return <ProjectCard key={index} project={project} />;
         })}
       </div>
     </section>
-  )
+  );
 }
